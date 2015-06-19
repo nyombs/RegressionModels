280ff334e1aedd9dea5cb0b26dcaa44fff17888c
Stephane Nyombayire <snyomba@microsoft.com>
2015-06-19 10:53:06 -0700

    Course Project: Regression Models
    Submit

--- SimpleLinearRegression.R
+++ SimpleLinearRegression.R
@@ -128,3 +128,40 @@ predict(fit, newdata, interval=("prediction"))
 fit_num <- lm(mpg ~ wt, data=mtcars)
 fit_denom <- lm(mpg ~ 1, data=mtcars)
 sum(resid(fit_num)^2) / sum(resid(fit_denom)^2)
+
+
+#Quiz 3:
+# 1 Consider the mtcars data set. Fit a model with mpg as the outcome that includes number of cylinders as a factor variable and weight as confounder. Give the adjusted
+# estimate for the expected change in mpg comparing 8 cylinders to 4.
+data(mtcars)
+summary(mtcars)
+str(mtcars)
+newCars <- mtcars
+newCars$cyl <- as.factor(newCars$cyl)
+# reference factor to compare the change to
+cyl4 <- relevel(newCars$cyl, "4")
+x <- lm(mpg ~ cyl4 + wt, data = newCars)
+#x <- lm(mpg ~ I(1 * (cyl == 8)) + I(1 * (cyl == 6)) * wt, data = newCars)
+
+# 2 Consider the mtcars data set. Fit a model with mpg as the outcome that includes number of cylinders as a factor variable 
+# and weight as a possible confounding variable. Compare the effect of 8 versus 4 cylinders on mpg for the adjusted and unadjusted by weight models. 
+# Here, adjusted means including the weight variable as a term in the regression model and unadjusted means the model without weight included. 
+# What can be said about the effect comparing 8 and 4 cylinders after looking at models with and without weight included?.
+summary(x)$coef
+
+x <- lm(mpg ~ cyl4 , data = newCars)
+summary(x)$coef
+
+# Consider the mtcars data set. Fit a model with mpg as the outcome that considers number of cylinders as a factor variable and weight as confounder. 
+# Now fit a second model with mpg as the outcome model that considers the interaction between number of cylinders (as a factor variable) and weight. 
+# Give the P-value for the likelihood ratio test comparing the two models and suggest a model using 0.05 as a type I error rate significance benchmark.
+x <- lm(mpg ~ factor(cyl) + wt, data = newCars)
+y <- lm(mpg ~ factor(cyl) * wt, data = newCars)
+
+anova(x, y)$"Pr(>F)"
+
+x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
+y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
+fit <- lm(y ~ x)
+hatvalues(fit)
+dfbetas(fit)
